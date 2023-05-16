05daad5c9fd7bc5a072c3efcb82e458d977f8d6f
Michael Bayer <yoyomike13@gmail.com>
2023-05-16 21:51:42 +0200

    Define standards for the password

--- PasswordChecker.cs
+++ PasswordChecker.cs
@@ -4,6 +4,11 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello World");
+      // Defining standards for the password
+      int minLength = 8;
+      string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string lowercase = "abcdefghijklmnopqrstuvwxyz";
+      string digits = "0123456789";
+      string specialChars = "#!%$&";
     }
 }
