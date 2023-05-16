5e5cd369bc4f9c17ffd0eb8d7b34626b1d7e8ef3
Michael Bayer <yoyomike13@gmail.com>
2023-05-16 21:53:59 +0200

    user input + score variable declared

--- PasswordChecker.cs
+++ PasswordChecker.cs
@@ -10,5 +10,11 @@ class Program
       string lowercase = "abcdefghijklmnopqrstuvwxyz";
       string digits = "0123456789";
       string specialChars = "#!%$&";
+
+      // Asking the user for a password
+      string userPassword = Console.ReadLine();
+
+      // Declaring a variable for the score of the password
+      int score = 0;
     }
 }
